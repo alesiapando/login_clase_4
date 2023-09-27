bff6208b370b6d349c480c746415cb6b73b06b69
alesiapando <138126244+alesiapando@users.noreply.github.com>
2023-09-27 19:17:12 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,13 @@
+<?php
+    $usuario = $_POST ["usuario"];
+    $contrasenia = $_POST ["pass"];
+    
+    $ckusuario= "admin";
+    $ckpass= 1234;
+
+    if ($usuario==$ckusuario && $contrasenia==$ckpass) {
+        header ("location:https://www.potrerodigital.org/");
+    } else {
+        header ("location:https://www.pixar.com/error404/");
+    }
+?>
